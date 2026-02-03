4a897157ee3244be87183c34fbac638386a1f942
swathisura <swathisura65@gmail.com>
2026-02-03 16:50:10 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,8 @@
-
+provider "aws" {
+ region = "us-east-1" 
+}
+resource "aws_launch_template" "backend" {
+  name_prefix   = "axy-backend"
+  image_id      = "ami-0c02fb55956c7d316"
+  instance_type = "t3.micro"
+}
